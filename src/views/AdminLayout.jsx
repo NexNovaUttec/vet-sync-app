7d116c9561a9ea26ce7f906a9cbd8fa2282ebbe9
omancillav <edgarmncllav04@gmail.com>
2026-03-01 14:11:29 -0600

    feat: update scrollbar styles for improved UI consistency in AdminLayout

--- src/views/AdminLayout.jsx
+++ src/views/AdminLayout.jsx
@@ -29,7 +29,7 @@ export function AdminLayout() {
       <Sidebar />
 
       {/* Main Content Area */}
-      <main className="flex-1 flex flex-col h-full overflow-y-auto">
+      <main className="flex-1 flex flex-col h-full overflow-y-auto [&::-webkit-scrollbar]:thin [-ms-overflow-style:none] [scrollbar-width:thin]">
         <div className="container max-w-7xl mx-auto px-4 py-2 sm:px-4 lg:p-8">
           {/* Mobile inline control (scrolls away with page) */}
           <div className="md:hidden mb-6 mt-1 flex flex-col gap-2">
